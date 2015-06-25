17d467106651fe8432bd7347dfd64db7a5d7327c
Zear <zeartul@gmail.com>
2015-06-25 22:23:34 +0200

    Increase the version number to 0.1.0

--- include/shake.h
+++ include/shake.h
@@ -6,8 +6,8 @@ extern "C" {
 #endif
 
 #define SHAKE_MAJOR_VERSION 0
-#define SHAKE_MINOR_VERSION 0
-#define SHAKE_PATCH_VERSION 9
+#define SHAKE_MINOR_VERSION 1
+#define SHAKE_PATCH_VERSION 0
 
 struct Shake_Device;
 typedef struct Shake_Device Shake_Device;
